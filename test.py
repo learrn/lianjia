bfdf83a82a999bf7e29b5597a87547f5a77d0644
xiangjuncheng <xiangjuncheng68@163.com>
2017-08-19 04:10:46 +0800

    change

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import random
 import re
 import time
 
-import BeautifulSoup
+import bs4
 import requests
 
-from LianJia.items import LianjiaItem
+from items import LianjiaItem
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 
 user_agent = [
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
@@ -28,54 +28,76 @@ headers = {
     'User-Agent': user_agent[random.randint(0, 5)]
 }
 
-csv_file = open("rent.csv", "wb")
-csv_writer = csv.writer(csv_file, delimiter=',')
-first_line=True
+first_line = True
 
 
 def get_latitude(url):  # 进入每个房源链接抓经纬度
     time.sleep(1)
     p = requests.get(url, headers=headers)
     html = p.text
-    content = BeautifulSoup.BeautifulSoup(html)
+    content = bs4.BeautifulSoup(html, "lxml")
     longitude = content.find('div', {'class': 'around js_content'})['longitude']
     latitude = content.find('div', {'class': 'around js_content'})['latitude']
     return [longitude, latitude]
 
 
-for i in range(1, 100):
-    url = 'http://sh.lianjia.com/zufang/d{}/'.format(str(i))
-    print url
-    try:
+def parse():
+    url = 'http://sh.lianjia.com/zufang/'
+    contents = requests.get(url, headers=headers)
+    contents.encoding = 'utf-8'
+    html = contents.text
+    content = bs4.BeautifulSoup(html, "lxml")
+    area_list = content.find('div', {'class': 'option-list gio_district'}).findAll('a')[1:-1]
+    for area in area_list:
+        # try:
+        area_url = 'http://sh.lianjia.com/zufang/{}'.format(area['gahref'])
+        detail_url(area_url)
+        # yield scrapy.Request(url=area_url, headers=headers, callback=self.detail_url,
+        #                      meta={"id1": area_han, "id2": area_pin})
+        # except Exception:
+        # pass
+
+
+def detail_url(area_url):
+    for i in range(1, 2):
+        url = area_url + '/d{}'.format(str(i))
+        print(url)
+        # try:
         time.sleep(1)
         contents = requests.get(url, headers=headers)
         contents.encoding = 'utf-8'
         html = contents.text
-        content = BeautifulSoup.BeautifulSoup(html)
+        content = bs4.BeautifulSoup(html, "lxml")
         houselist = content.findAll('div', {'class': 'info-panel'})
         for house in houselist:
-            try:
-                item = LianjiaItem()
-                item['title'] = house.find('h2').find('a')['title']
-                item['community'] = house.find('a', {'class': 'laisuzhou'}).find('span')['title']
-                a = house.find('div', {'class': 'where'}).text.split('&nbsp;&nbsp;')
-                item['model'] = a[-3]
-                item['area'] = a[-2]
-                # item['focus_num'] = house.xpath('div[1]/div[4]/text()').pop().split('/')[0]
-                # item['watch_num'] = house.xpath('div[1]/div[4]/text()').pop().split('/')[1]
-                item['time'] = house.find('div', {'class': 'price-pre'}).text.replace('\n', '').replace('\t', '')
-                item['price'] = house.find('div', {'class': 'price'}).find('span', {'class': 'num'}).text
-                # item['average_price'] = house.xpath('div[1]/div[6]/div[2]/span/text()').pop()
-                url_detail = url.replace('/zufang/d{}/'.format(str(i)), house.find('h2').find('a')['href'])
-                item['link'] = house.find('h2').find('a')['href']
-                # # item['city'] = response.meta["id1"]
-                item['Latitude'] = get_latitude(url_detail)
-                if first_line:
-                    first_line=False
-                    csv_writer.writerow(item.keys())
-                csv_writer.writerow(item.values())
-            except Exception:
-                print Exception.message
-    except Exception:
-        print Exception.message
+            #    try:
+            item = LianjiaItem()
+            item['title'] = house.find('h2').find('a')['title']
+            item['community'] = house.find('a', {'class': 'laisuzhou'}).find('span')['title']
+            a = house.find('div', {'class': 'where'}).text.split('\n')
+            item['model'] = a[-3].replace('\t', '').replace(' ', '')
+            item['area'] = a[-2].replace('\t', '').replace(' ', '')
+            # item['focus_num'] = house.xpath('div[1]/div[4]/text()').pop().split('/')[0]
+            # item['watch_num'] = house.xpath('div[1]/div[4]/text()').pop().split('/')[1]
+            item['time'] = house.find('div', {'class': 'price-pre'}).text.replace('\n', '').replace('\t', '')
+            item['price'] = house.find('div', {'class': 'price'}).find('span', {'class': 'num'}).text
+            # item['average_price'] = house.xpath('div[1]/div[6]/div[2]/span/text()').pop()
+            url_detail = 'http://sh.lianjia.com' + house.find('h2').find('a')['href']
+            item['link'] = house.find('h2').find('a')['href']
+            item['Latitude'] = get_latitude(url_detail)
+            # if first_line:
+            #     first_line = False
+            #     csv_writer.writerow(item.keys())
+            b = item.values()
+            csv_writer.writerow([item['title'], item['community'], item['model'], item['area'], item['price'],
+                                item['time'], item['link'], item['Latitude']])
+            # except Exception:
+            #    print(Exception.message)
+            # except Exception:
+            #    print(Exception.message)
+
+
+csv_file = open("rent.csv", "wb")
+csv_writer = csv.writer(csv_file, delimiter=',')
+parse()
 csv_file.close()
